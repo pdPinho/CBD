9554a749a9c6119430bd3b52c69475caf4da164c
pdPinho <pd.pinho@ua.pt>
2023-09-24 21:24:40 +0100

    finished I hope

---
+++ guiao_01/ex05/README.md
@@ -0,0 +1,10 @@
+During this exercise I found a lot of problems / difficulties when solving it. Specially when it came to the logic of how to do it.
+
+As we can observe, exercise 1.5 a) was solved in an inefficient way and pretty ugly.
+It could've been solved with the usage of multiple entries.
+
+The same thing happens for 1.5 b). Even though it is a lot better than the previous exercise we sadly encounter a problem which I only noticed a little bit too late during it's development (Sorted sets are based on score and quantities don't work well with the way I solved it).
+So even though it works, it only half works due to it's sorting.
+This was caused by misinterpreting the documentation.
+
+Lastly we have the 1.5 b) version_2 which is simpler but gets the job done.
--- guiao_01/ex05/atendimento_b/src/main/java/redis/Post.java
+++ guiao_01/ex05/atendimento_b/src/main/java/redis/Post.java
@@ -1,6 +1,8 @@
 package redis;
 
 import java.util.*;
+import java.util.concurrent.TimeUnit;
+
 import redis.clients.jedis.Jedis;
  
 public class Post {
@@ -8,35 +10,60 @@ public class Post {
 	private Jedis jedis;
 	public static String REQUESTS = "requests by ";
 	public static final int MAX_QUANTITY = 20;
-	public static final int TS = 1; // seconds
+	public static final int TS = 10; // seconds
 	
 	public Post() {
 		this.jedis = new Jedis();
 	}
  
 	// Saves request made by user with given product and quantity
-	public void saveRequest(String username, Map<String, Double> product) {
-		if(canRequest(username, 1)){
-			jedis.zadd(username, product);
-			jedis.lpush(REQUESTS + username, Long.toString(System.currentTimeMillis() / 1000));
+	public void saveRequest(String username, Map<String, Double> request, double quantity) {
+		try {
+			checkTimeLimit(username);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		if(canRequest(username, quantity)){
+			jedis.rpush(REQUESTS + username, Long.toString(System.currentTimeMillis() / 1000));
+			jedis.zadd(username, request);
 		}
 	}
 
-	// Returns products from given user
-	public List<String> getProducts(String username){
+	// Clean requests made by given user
+	public void cleanRequests(String username){
+		jedis.del(REQUESTS + username);
+		jedis.del(username);
+	}
+
+	// Clean lists based on time limit
+	public void checkTimeLimit(String username){
+		long currentTime = System.currentTimeMillis() / 1000;
+		for (String time : getTimestamps(username))
+			if(currentTime >= Long.parseLong(time) + TS)
+				jedis.lpop(REQUESTS + username);
+	}
+
+	// Removes timestamp due to overwrite
+	public void removeTimestamp(int ts, String username){
+		jedis.lrem(REQUESTS + username, 1, jedis.lindex(REQUESTS + username, ts));
+	}
+
+	// Returns requests from given user
+	public List<String> getRequests(String username){
 		return jedis.zrange(username, 0, -1);
 	}
 
 	// Returns requests timestamps for given user
-	public List<String> getRequests(String username){
-		return jedis.lrange(REQUESTS + username, 0, 100);
+	public List<String> getTimestamps(String username){
+		return jedis.lrange(REQUESTS + username, 0, -1);
 	}
 
 	// Verifies if given user hasn't reached its limit
-	public boolean canRequest(String username, int quantity){
+	public boolean canRequest(String username, double quantity){
 		int totalQuantity = 0;
 
-		for(String product: getProducts(username)){
+		for(String product: getRequests(username)){
 			totalQuantity += jedis.zmscore(username, product).get(0);
 		}
 
@@ -44,29 +71,85 @@ public class Post {
 			return true;
 		return false;
 	}
+
+	// Sleep for given time
+	public void sleepApp(int time){
+		try {
+			TimeUnit.SECONDS.sleep(time);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
  
 	public static void main(String[] args) {
 		Post handler = new Post();
 
+		handler.cleanRequests("Jane");
+		handler.cleanRequests("Artemis");
+
 		Map<String, Double> request = new HashMap<>();
+		String user = "Jane";
+		double quantity;
+		String product;
 
-		request.put("Apple", 2.0);
-		handler.saveRequest("Jane", request);
-		request.put("Banana", 5.0);
-		handler.saveRequest("Jane", request);
-		request.put("Apple", 20.0);
-		handler.saveRequest("Jane", request);
+		// Example of insertions
+		product = "Apple";
+		quantity = 2.0;
+		request.put(product, quantity);
+		handler.saveRequest(user, request, quantity);
+		handler.sleepApp(1);
 
-		System.out.println(handler.getProducts("Jane"));
+		product = "Banana";
+		quantity = 5.0;
+		request.put(product, quantity);
+		handler.saveRequest(user, request, quantity);
+		handler.sleepApp(1);
+
+		product = "Apple";
+		quantity = 5.0;
+		if(handler.getRequests(user).contains(product)){
+			request.put(product, request.get(product) + quantity);
+
+			handler.removeTimestamp(handler.getRequests(user).indexOf(product), user);
+			handler.saveRequest(user, request, quantity);
+		}
+
 		System.out.println(handler.getRequests("Jane"));
+		System.out.println(handler.getTimestamps("Jane"));
+		handler.sleepApp(1);
 
-		/*
+		product = "Banana";
+		quantity = 3.0;
+		if(handler.getRequests(user).contains(product)){
+			request.put(product, request.get(product) + quantity);
+
+			handler.removeTimestamp(handler.getRequests(user).indexOf(product), user);
+			handler.saveRequest(user, request, quantity);
+		}
+
+		handler.sleepApp(1);
+		
+		product = "Melon";
+		quantity = 1.0;
+		request.put(product, quantity);
+		handler.saveRequest(user, request, quantity);
+		handler.sleepApp(1);
+		System.out.println(handler.getRequests("Jane"));
+		System.out.println(handler.getTimestamps("Jane"));
+
+		product = "Kiwi";
+		quantity = 1.0;
+		request.put(product, quantity);
+		handler.saveRequest(user, request, quantity);
+		System.out.println(handler.getRequests("Jane"));
+		System.out.println(handler.getTimestamps("Jane"));
+
+		
 		request = new HashMap<>();
 		request.put("Watermelon", 9.0);
-		handler.saveRequest("Artemis", request);
+		handler.saveRequest("Artemis", request, 9.0);
 
-		System.out.println(handler.getProducts("Artemis"));
-		*/
+		System.out.println(handler.getRequests("Artemis"));
 	}
 }
 
